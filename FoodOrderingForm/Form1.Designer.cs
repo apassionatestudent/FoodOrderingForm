a3df2e2efee963a6b5f66960161a8af9e5799444
Ricson Escalicas <67963666+apassionatestudent@users.noreply.github.com>
2024-08-16 08:02:02 +0800

    Add files via upload

---
+++ FoodOrderingForm/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace FoodOrderingForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPaymentMethod = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.cmbPaymentMethod = new System.Windows.Forms.ComboBox();
+            this.radDineIn = new System.Windows.Forms.RadioButton();
+            this.radTakeOut = new System.Windows.Forms.RadioButton();
+            this.lblMenu = new System.Windows.Forms.Label();
+            this.ckbChicken = new System.Windows.Forms.CheckBox();
+            this.lblBurgers = new System.Windows.Forms.Label();
+            this.ckbBeef = new System.Windows.Forms.CheckBox();
+            this.ckbPork = new System.Windows.Forms.CheckBox();
+            this.lblDrinks = new System.Windows.Forms.Label();
+            this.ckbCoke = new System.Windows.Forms.CheckBox();
+            this.ckbSprite = new System.Windows.Forms.CheckBox();
+            this.ckbRoyal = new System.Windows.Forms.CheckBox();
+            this.lblFries = new System.Windows.Forms.Label();
+            this.ckbSpicy = new System.Windows.Forms.CheckBox();
+            this.ckbSweetSpicy = new System.Windows.Forms.CheckBox();
+            this.ckbRegular = new System.Windows.Forms.CheckBox();
+            this.lblCurrentTotal = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.lblOrderDetails = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            resources.ApplyResources(this.lblTitle, "lblTitle");
+            this.lblTitle.Name = "lblTitle";
+            // 
+            // lblName
+            // 
+            resources.ApplyResources(this.lblName, "lblName");
+            this.lblName.Name = "lblName";
+            // 
+            // lblPaymentMethod
+            // 
+            resources.ApplyResources(this.lblPaymentMethod, "lblPaymentMethod");
+            this.lblPaymentMethod.Name = "lblPaymentMethod";
+            // 
+            // txtName
+            // 
+            resources.ApplyResources(this.txtName, "txtName");
+            this.txtName.Name = "txtName";
+            // 
+            // cmbPaymentMethod
+            // 
+            this.cmbPaymentMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPaymentMethod.FormattingEnabled = true;
+            this.cmbPaymentMethod.Items.AddRange(new object[] {
+            resources.GetString("cmbPaymentMethod.Items"),
+            resources.GetString("cmbPaymentMethod.Items1"),
+            resources.GetString("cmbPaymentMethod.Items2")});
+            resources.ApplyResources(this.cmbPaymentMethod, "cmbPaymentMethod");
+            this.cmbPaymentMethod.Name = "cmbPaymentMethod";
+            // 
+            // radDineIn
+            // 
+            resources.ApplyResources(this.radDineIn, "radDineIn");
+            this.radDineIn.Name = "radDineIn";
+            this.radDineIn.TabStop = true;
+            this.radDineIn.UseVisualStyleBackColor = true;
+            this.radDineIn.CheckedChanged += new System.EventHandler(this.radDineIn_CheckedChanged);
+            // 
+            // radTakeOut
+            // 
+            resources.ApplyResources(this.radTakeOut, "radTakeOut");
+            this.radTakeOut.Name = "radTakeOut";
+            this.radTakeOut.TabStop = true;
+            this.radTakeOut.UseVisualStyleBackColor = true;
+            this.radTakeOut.CheckedChanged += new System.EventHandler(this.radTakeOut_CheckedChanged);
+            // 
+            // lblMenu
+            // 
+            resources.ApplyResources(this.lblMenu, "lblMenu");
+            this.lblMenu.Name = "lblMenu";
+            // 
+            // ckbChicken
+            // 
+            resources.ApplyResources(this.ckbChicken, "ckbChicken");
+            this.ckbChicken.Name = "ckbChicken";
+            this.ckbChicken.UseVisualStyleBackColor = true;
+            // 
+            // lblBurgers
+            // 
+            resources.ApplyResources(this.lblBurgers, "lblBurgers");
+            this.lblBurgers.Name = "lblBurgers";
+            // 
+            // ckbBeef
+            // 
+            resources.ApplyResources(this.ckbBeef, "ckbBeef");
+            this.ckbBeef.Name = "ckbBeef";
+            this.ckbBeef.UseVisualStyleBackColor = true;
+            // 
+            // ckbPork
+            // 
+            resources.ApplyResources(this.ckbPork, "ckbPork");
+            this.ckbPork.Name = "ckbPork";
+            this.ckbPork.UseVisualStyleBackColor = true;
+            // 
+            // lblDrinks
+            // 
+            resources.ApplyResources(this.lblDrinks, "lblDrinks");
+            this.lblDrinks.Name = "lblDrinks";
+            // 
+            // ckbCoke
+            // 
+            resources.ApplyResources(this.ckbCoke, "ckbCoke");
+            this.ckbCoke.Name = "ckbCoke";
+            this.ckbCoke.UseVisualStyleBackColor = true;
+            // 
+            // ckbSprite
+            // 
+            resources.ApplyResources(this.ckbSprite, "ckbSprite");
+            this.ckbSprite.Name = "ckbSprite";
+            this.ckbSprite.UseVisualStyleBackColor = true;
+            // 
+            // ckbRoyal
+            // 
+            resources.ApplyResources(this.ckbRoyal, "ckbRoyal");
+            this.ckbRoyal.Name = "ckbRoyal";
+            this.ckbRoyal.UseVisualStyleBackColor = true;
+            // 
+            // lblFries
+            // 
+            resources.ApplyResources(this.lblFries, "lblFries");
+            this.lblFries.Name = "lblFries";
+            // 
+            // ckbSpicy
+            // 
+            resources.ApplyResources(this.ckbSpicy, "ckbSpicy");
+            this.ckbSpicy.Name = "ckbSpicy";
+            this.ckbSpicy.UseVisualStyleBackColor = true;
+            // 
+            // ckbSweetSpicy
+            // 
+            resources.ApplyResources(this.ckbSweetSpicy, "ckbSweetSpicy");
+            this.ckbSweetSpicy.Name = "ckbSweetSpicy";
+            this.ckbSweetSpicy.UseVisualStyleBackColor = true;
+            // 
+            // ckbRegular
+            // 
+            resources.ApplyResources(this.ckbRegular, "ckbRegular");
+            this.ckbRegular.Name = "ckbRegular";
+            this.ckbRegular.UseVisualStyleBackColor = true;
+            // 
+            // lblCurrentTotal
+            // 
+            resources.ApplyResources(this.lblCurrentTotal, "lblCurrentTotal");
+            this.lblCurrentTotal.Name = "lblCurrentTotal";
+            // 
+            // lblResult
+            // 
+            resources.ApplyResources(this.lblResult, "lblResult");
+            this.lblResult.Name = "lblResult";
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.BackColor = System.Drawing.Color.ForestGreen;
+            resources.ApplyResources(this.btnOrder, "btnOrder");
+            this.btnOrder.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.UseVisualStyleBackColor = false;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.IndianRed;
+            resources.ApplyResources(this.btnClear, "btnClear");
+            this.btnClear.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnClear.Name = "btnClear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            resources.ApplyResources(this.lstBox, "lstBox");
+            this.lstBox.Name = "lstBox";
+            // 
+            // lblOrderDetails
+            // 
+            resources.ApplyResources(this.lblOrderDetails, "lblOrderDetails");
+            this.lblOrderDetails.Name = "lblOrderDetails";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.Controls.Add(this.lblOrderDetails);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnOrder);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblCurrentTotal);
+            this.Controls.Add(this.ckbRegular);
+            this.Controls.Add(this.ckbSweetSpicy);
+            this.Controls.Add(this.ckbSpicy);
+            this.Controls.Add(this.lblFries);
+            this.Controls.Add(this.ckbRoyal);
+            this.Controls.Add(this.ckbSprite);
+            this.Controls.Add(this.ckbCoke);
+            this.Controls.Add(this.lblDrinks);
+            this.Controls.Add(this.ckbPork);
+            this.Controls.Add(this.ckbBeef);
+            this.Controls.Add(this.lblBurgers);
+            this.Controls.Add(this.ckbChicken);
+            this.Controls.Add(this.lblMenu);
+            this.Controls.Add(this.radTakeOut);
+            this.Controls.Add(this.radDineIn);
+            this.Controls.Add(this.cmbPaymentMethod);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblPaymentMethod);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPaymentMethod;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.RadioButton radDineIn;
+        private System.Windows.Forms.RadioButton radTakeOut;
+        private System.Windows.Forms.Label lblMenu;
+        private System.Windows.Forms.CheckBox ckbChicken;
+        private System.Windows.Forms.Label lblBurgers;
+        private System.Windows.Forms.CheckBox ckbBeef;
+        private System.Windows.Forms.CheckBox ckbPork;
+        private System.Windows.Forms.Label lblDrinks;
+        private System.Windows.Forms.CheckBox ckbCoke;
+        private System.Windows.Forms.CheckBox ckbSprite;
+        private System.Windows.Forms.CheckBox ckbRoyal;
+        private System.Windows.Forms.Label lblFries;
+        private System.Windows.Forms.CheckBox ckbSpicy;
+        private System.Windows.Forms.CheckBox ckbSweetSpicy;
+        private System.Windows.Forms.CheckBox ckbRegular;
+        private System.Windows.Forms.Label lblCurrentTotal;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Label lblOrderDetails;
+        private System.Windows.Forms.ComboBox cmbPaymentMethod;
+    }
+}
+
---
+++ FoodOrderingForm/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FoodOrderingForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeItemPrices();
+        }
+        Dictionary<CheckBox, int> itemPrices;
+        private void InitializeItemPrices()
+        {
+            itemPrices = new Dictionary<CheckBox, int>
+            {
+                { ckbChicken, 50 },
+                { ckbBeef, 60 },
+                { ckbPork, 80 },
+                { ckbCoke, 10 },
+                { ckbSprite, 10 },
+                { ckbRoyal, 10 },
+                { ckbSpicy, 15 },
+                { ckbSweetSpicy, 20 },
+                { ckbRegular, 10 }
+            };
+
+            ckbChicken.CheckedChanged += CheckedChanged;
+            ckbBeef.CheckedChanged += CheckedChanged;
+            ckbPork.CheckedChanged += CheckedChanged;
+            ckbCoke.CheckedChanged += CheckedChanged;
+            ckbSprite.CheckedChanged += CheckedChanged;
+            ckbRoyal.CheckedChanged += CheckedChanged;
+            ckbSpicy.CheckedChanged += CheckedChanged;
+            ckbSweetSpicy.CheckedChanged += CheckedChanged;
+            ckbRegular.CheckedChanged += CheckedChanged;
+        }
+
+
+
+        string orders = string.Empty;
+        int result = 0, total = 0;
+        string dine = string.Empty;
+
+        private void CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkbox = sender as CheckBox;
+            if (checkbox.Checked)
+            {
+                result += itemPrices[checkbox] ;
+                total += itemPrices[checkbox] ;
+                orders += $"P{itemPrices[checkbox]} - {checkbox.Text} \n";
+                lstBox.Items.Add($"{checkbox.Text} \n");
+            }
+            else
+            {
+                result -= itemPrices[checkbox];
+                total -= itemPrices[checkbox];
+                orders = orders.Replace($"P{itemPrices[checkbox]} - {checkbox.Text} \n", "");
+                for (int i = lstBox.Items.Count - 1; i >= 0; i--)
+                {
+                    if (lstBox.Items[i].ToString().Trim() == checkbox.Text)
+                    {
+                        lstBox.Items.RemoveAt(i);
+                    }
+                }
+            }
+            lblResult.Text = "₱" + result.ToString();
+        }
+
+        private void radTakeOut_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radTakeOut.Checked)
+            {
+                dine = "Take out";
+            }
+        }
+
+        private void radDineIn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radDineIn.Checked)
+            {
+                dine = "Dine In";
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            orders = String.Empty;
+            lblResult.Text = "₱0";
+            txtName.Text = String.Empty;
+            cmbPaymentMethod.Text = null;
+            radDineIn.Checked = false;
+            radTakeOut.Checked = false;
+            ckbBeef.Checked = false;
+            ckbChicken.Checked = false;
+            ckbPork.Checked = false;
+            ckbCoke.Checked = false;
+            ckbSprite.Checked = false;
+            ckbRoyal.Checked = false;
+            ckbSpicy.Checked = false;
+            ckbSweetSpicy.Checked = false;
+            ckbRegular.Checked = false;
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                bool anyChecked = itemPrices.Any(pair => pair.Key.Checked);
+
+                if (!anyChecked)
+                {
+                    throw new Exception("Please select at least one item.");
+                }
+
+                if (string.IsNullOrEmpty(txtName.Text) || cmbPaymentMethod.SelectedIndex == -1 || (radDineIn.Checked == false && radTakeOut.Checked == false))
+                {
+                    throw new Exception("Please fill out all the necessary data to process your order.");
+                }
+
+
+                DateTime currentDateTime = DateTime.Now; 
+                string receipt = $"WONDERFUL BURGERS INC.\nAddress: 7WR4+X7J, A. Pigafetta Street, Cebu City, 6000 Cebu \nDate: {currentDateTime} \n\n" +
+                    $"Hi {txtName.Text}!" +
+                    $"\nOrder Details:\n\nPayment Method: {cmbPaymentMethod.SelectedItem} \nDine: {dine} \nOrders: \n{orders} \nTotal: P{total}.";
+                string message = "RECEIPT";
+
+                DialogResult resultDialog = MessageBox.Show(receipt, message, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                if (resultDialog == DialogResult.OK)
+                {
+                    btnClear_Click(sender, e); 
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = $"Error: {ex.Message} \n \nThank you!";
+                string message = "Error";
+
+                MessageBox.Show(error, message, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    } 
+ }
